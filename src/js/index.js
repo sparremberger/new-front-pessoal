5520ccc42a83eed5a8f996db457cc5b7097d294e
Alan Sparremberger <alan.rs@outlook.com>
2021-07-06 03:58:48 -0300

    .

--- src/js/index.js
+++ src/js/index.js
@@ -18,14 +18,29 @@ document.addEventListener("keydown", function (event) {
 });
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navigation").style.top = "0";
-  } else {
-    document.getElementById("navigation").style.top = "-6em";
-  }
-  prevScrollpos = currentScrollPos;
-} 
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navigation").style.top = "0";
+    } else {
+        document.getElementById("navigation").style.top = "-6em";
+    }
+    prevScrollpos = currentScrollPos;
+};
 
-console.log(document.getElementById("#experiencia > ul > li:nth-child(4)"));
+/* Dá pra refatorar legal isso. */
+function showDescription(work) {
+    if (work == "cent") {
+        document.getElementsByClassName("wk1")[0].style.display = "none";
+        document.getElementsByClassName("wk3")[0].style.display = "none";
+        document.getElementsByClassName("wk2")[0].style.display = "inline-block";
+    } else if (work == "hnsc") {
+        document.getElementsByClassName("wk2")[0].style.display = "none";
+        document.getElementsByClassName("wk3")[0].style.display = "none";
+        document.getElementsByClassName("wk1")[0].style.display = "inline-block";
+    } else if (work == "tec") {
+        document.getElementsByClassName("wk1")[0].style.display = "none";
+        document.getElementsByClassName("wk2")[0].style.display = "none";
+        document.getElementsByClassName("wk3")[0].style.display = "inline-block";
+    }
+}
